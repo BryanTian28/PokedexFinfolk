74c920f0685e7eb26efc2a2b731d0ab31af1acfc
BryanTian28 <austinbryan2000@gmail.com>
2025-10-15 18:01:07 +0700

    Updated API Pointing to updated and added env example

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://graphqlpokemon.favware.tech/v7',
+  uri: 'https://graphqlpokemon.favware.tech/v8',
   cache: new InMemoryCache(),
 });
 
